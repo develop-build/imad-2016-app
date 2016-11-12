84659cc7149b7228118f7ee974be7504a597688c
Anubhav Jain <anubhavj1357@gmail.com>
2016-11-12 23:31:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/main.js', function(){
+app.get('/ui/main.js', function(req,res){
    res.sendFile(path.join(_dirname, 'ui', 'main.js')); 
 });
 
