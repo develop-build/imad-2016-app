dbe8aaf8f3299c8ea3419b59c93b07e9f98845c8
Anubhav Jain <anubhavj1357@gmail.com>
2016-11-13 08:06:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,9 +5,9 @@ console.log('Loaded!');
 var marginLeft = 0;
 var image = document.getElementById('image');
 function moveRight(){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 5;
     image.style.marginLeft = marginLeft+'px';
 }
 image.onclick = function(){
-  var interval = setInterval(moveRight,100) ;
+  var interval = setInterval(moveRight,50) ;
 };
