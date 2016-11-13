86fff05f2dd2d282b3a3c76e7cf33394bfe7347b
Anubhav Jain <anubhavj1357@gmail.com>
2016-11-13 08:41:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,10 +83,10 @@ app.get('/counter', function(req ,res){
    res.send(counter.toString()) ;
 });
 
-/*app.get('/:articleName',function(req,res){
+app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
-});*/
+});
 
 
 app.get('/ui/style.css', function (req, res) {
