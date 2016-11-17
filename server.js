f3c55a658fa467febc3543f13f86e8ab0afa684a
Anubhav Jain <anubhavj1357@gmail.com>
2016-11-17 18:29:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,42 +1,22 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+
+var config = {
+	user:'develop-build',
+	database: 'develop-build',
+	host: 'db.imad.hasura-app.io',
+	port: '5432',
+	password: process.env.DB_PASSWORD
+};
+
 
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-    'article-one' : {
-    title:'Article One | Anubhav',
-    heading:'Article One',
-    date : 'Sep 5 2016',
-    content : `<p>
-                    This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.
-                </p>
-                <p>
-                    This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.
-                </p>
-                <p>
-                    This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.This content to first article.
-                </p>`
-},
-    'article-two' :{
-        title:'Article Two | Anubhav',
-        heading:'Article Two',
-        date : 'Sep 10 2016',
-        content :`<p>
-                    This content to second article.This content to second article..
-                </p>`
-    },
-    'article-three':{
-        title:'Article Three | Anubhav',
-        heading:'Article Three',
-        date : 'Sep 12 2016',
-        content :`<p>
-                    This content to third article.This content to third article..
-                </p>`
-    }
-};
+
 function createTemplate(data){
     var title = data.title;
     var date = data.date;
@@ -60,7 +40,7 @@ function createTemplate(data){
                     <hr>
                     <h3>${heading}</h3>
                     <div>
-                        ${date}
+                        ${date.toDateString()}
                     </div>
                     <div>
                         ${content}
@@ -72,29 +52,72 @@ function createTemplate(data){
         return(htmlTemplate);
 }
 
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+function hash(input, salt){
+    //using default library crypto used for adding a salt and generating the hashed string
+    //using password based key derivation funtion in crypto library
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return ["pbkdf2","10000", salt, hashed.toString('hex')].join('$');
+
+
+}
+
+app.get('/hash/:input', function(req, res){
+    var hashedString = hash(req.params.input , 'this-is-some-random-string');
+    res.send(hashedString);
+});
+
+app.get('/create-user', function(req, res){
+    
+});
+
+var pool = new Pool(config);
+app.get('/test-db',function(req, res){
+//make a select request
+//return a response with the results
+    pool.query('SELECT * FROM test',function(err,result){
+    if(err){
+            res.status(500).send(err.toString());
+	}else{
+	    res.send(JSON.stringify(result.rows));
+	}
+    });
+});
+
 var counter = 0;
-app.get('/counter', function(req, res){
+app.get('/counter', function(req ,res){
    counter = counter+1;
    res.send(counter.toString()) ;
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req, res){
-   //get name from request
-   var name = req.params.name; 
-   names.push(name);
-   //using json to convert the JS object(an array) to HTML string
-   res.send(JSON.stringify(names));
+app.get('/submit-name',function(req,res){
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName',function(req, res){
+app.get('/articles/:articleName',function(req,res){
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName],function(err,result){
+    
+    if(err){
+        res.status(500).send(err.toString());
+    }else
+
+    if(result.rows.length===0){
+        res.status(404).send('Article Not found');
+
+    }else
+    {
+        var articleData = result.rows[0];
+        res.send(createTemplate(articleData));
+    }
+    });
+
 });
 
 
@@ -116,7 +139,7 @@ app.get('/ui/anuDP.jpg', function (req, res) {
 
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
+var port = 8081; // Use 8081 for local development because you might already have apache running on 80
+app.listen(8081, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
